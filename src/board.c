0ef28e29b0f83a5eb095c75e8912ab1a618ba139
Kevin Mangan <kevin.m.mangan@gmail.com>
2020-08-29 15:56:49 -0700

    refactoring board

---
+++ src/board.c
@@ -0,0 +1,67 @@
+#include "board.h"
+
+Board* createBoard(int w, int h, int colors) {
+
+}
+
+
+
+int countMatches(&board, int x, int y, int xStep, int yStep) {
+  int matches = 0;
+  int xNext = x + xStep;
+  int yNext = y + yStep;
+  Dot* this = getCell(board, x, y);
+  Dot* that = getCell(board, xNext, yNext);
+
+  // check bounds
+  if (xNext >= board->w || xNext < 0 || yNext >= board->h || yNext < 0) {
+    return matches;
+  }
+
+  if (this->color == this->color) {
+    // count the match and then check for the next match, recursively
+    ++matches += countMatches(board, xNext, yNext, xStep, yStep);
+  }
+
+  return matches;
+}
+
+static void fall(void) {
+  int x, y;
+  for (x = 0; x < GRID_SIZE; x++) {
+    for (y = (GRID_SIZE-1); y > 0; y--) {
+      if (!grid[x][y]) { // if this cell is empty
+        if(grid[x][y-1]) { // check the above cell
+          // move its contents down a space
+          swapDots(x, y-1, x, y);
+          // Dot *dot = grid[x][y-1];
+          // float endX, endY;
+          // gridToScreen(x, y, &endX, &endY);
+          // grid[x][y] = dot;
+          // animateMoveTo(grid[x][y], endX, endY);
+          // grid[x][y-1] = NULL;
+        }
+      }
+    }
+  }
+
+  // top row
+  y = 0;
+  for (x = 0; x < GRID_SIZE; x++) {
+    if (!grid[x][y]) {
+      Dot *dot = createDot(x, y);
+      int endY = dot->y;
+      dot->y -= TILE_SIZE + TILE_MARGIN;
+      animateMoveTo(dot, dot->x, endY);
+      grid[x][y] = dot;
+    }
+  }
+}
+
+void destroyBoard(Board &board) {
+
+}
+
+Dot* getCell(Board &board, int x, int y) {
+  return board[x + (y * board->w)];
+}
