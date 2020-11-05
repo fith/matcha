8cce454e94b0159b4d8639d7b0d9b1beee05ec10
Kevin Mangan <kevin.m.mangan@gmail.com>
2020-11-04 18:13:03 -0800

    builds

---
+++ build/cmake-build-release/CMakeFiles/Matcha.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/kevin/Scratch/c/matcha/src/app/level1.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/app/level1.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/app/menu.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/app/menu.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/app/options.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/app/options.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/data/colors.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/data/colors.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/data/levels.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/data/levels.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/engine/draw.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/engine/draw.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/engine/init.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/engine/init.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/engine/input.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/engine/input.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/engine/sound.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/engine/sound.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/engine/text.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/engine/text.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/engine/util.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/engine/util.c.o"
+  "/Users/kevin/Scratch/c/matcha/src/main.c" "/Users/kevin/Scratch/c/matcha/build/cmake-build-release/CMakeFiles/Matcha.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/cmake-build-release/CMakeFiles/Matcha.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Matcha.dir/src/app/level1.c.o"
+  "CMakeFiles/Matcha.dir/src/app/menu.c.o"
+  "CMakeFiles/Matcha.dir/src/app/options.c.o"
+  "CMakeFiles/Matcha.dir/src/data/colors.c.o"
+  "CMakeFiles/Matcha.dir/src/data/levels.c.o"
+  "CMakeFiles/Matcha.dir/src/engine/draw.c.o"
+  "CMakeFiles/Matcha.dir/src/engine/init.c.o"
+  "CMakeFiles/Matcha.dir/src/engine/input.c.o"
+  "CMakeFiles/Matcha.dir/src/engine/sound.c.o"
+  "CMakeFiles/Matcha.dir/src/engine/text.c.o"
+  "CMakeFiles/Matcha.dir/src/engine/util.c.o"
+  "CMakeFiles/Matcha.dir/src/main.c.o"
+  "Matcha"
+  "Matcha.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Matcha.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
